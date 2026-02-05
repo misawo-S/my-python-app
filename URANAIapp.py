0b318bef03e7b4d3c1ee04eb51eab8f27e6097c7
misawo <misao.sayto@gmail.com>
2026-02-05 16:08:50 +0900

    uranaiapp修正版

--- URANAIapp.py
+++ URANAIapp.py
@@ -5,11 +5,11 @@ import random
 st.title("魔法のmeb占い")
 
 #ボタン設置
-if st.button("運勢を占う")
-kuji=["大吉","吉","凶"]
-result = random.choice(kuji)
+if st.button("運勢を占う"):
+ kuji=["大吉","吉","凶"]
+ result = random.choice(kuji)
 
 #画面に大きく表示
-st.header (f"結果は...{result}!!!")
-st.balloons()
+ st.header (f"結果は...{result}!!!")
+ st.balloons()
 
